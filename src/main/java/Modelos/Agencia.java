e86311a53356126d1819e7bab88426f7241e3f83
Stiven Medina <stvnm33@gmail.com>
2023-06-28 16:25:40 -0500

    Creando modelo para Agencia

--- src/main/java/Modelos/Agencia.java
+++ src/main/java/Modelos/Agencia.java
@@ -2,4 +2,70 @@ package Modelos;
 
 public class Agencia {
     
+    private int id;
+    private String nombre;
+    private String codigo_interno;
+    private String direccion;
+    private boolean estado;
+
+    public Agencia(int id, String nombre, String codigo_interno, String direccion, boolean estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.codigo_interno = codigo_interno;
+        this.direccion = direccion;
+        this.estado = estado;
+    }
+
+    public Agencia(String nombre, String codigo_interno, String direccion, boolean estado) {
+        this.nombre = nombre;
+        this.codigo_interno = codigo_interno;
+        this.direccion = direccion;
+        this.estado = estado;
+    }
+
+    public Agencia() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo_interno() {
+        return codigo_interno;
+    }
+
+    public void setCodigo_interno(String codigo_interno) {
+        this.codigo_interno = codigo_interno;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
 }
