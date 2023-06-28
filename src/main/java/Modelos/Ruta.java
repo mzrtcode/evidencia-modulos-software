0be0ca67c8b8812688ebcd141a60a8e96601a55c
Stiven Medina <stvnm33@gmail.com>
2023-06-28 16:19:06 -0500

    Creando modelo para Ruta

--- src/main/java/Modelos/Ruta.java
+++ src/main/java/Modelos/Ruta.java
@@ -2,4 +2,41 @@ package Modelos;
 
 public class Ruta {
     
+    private int id;
+    private String inicio;
+    private String destino;
+
+    public Ruta(String inicio, String destino) {
+        this.inicio = inicio;
+        this.destino = destino;
+    }
+
+    public Ruta() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(String inicio) {
+        this.inicio = inicio;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+    
+    
 }
