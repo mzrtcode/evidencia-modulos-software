46b738311850a30987b63218dd0d40111b78145d
Stiven Medina <stvnm33@gmail.com>
2023-06-28 16:38:44 -0500

    Añadiendo interfaz para Ruta

--- src/main/java/ModelosDao/IRuta.java
+++ src/main/java/ModelosDao/IRuta.java
@@ -1,5 +1,14 @@
 package ModelosDao;
 
+import Modelos.Ruta;
+import java.util.List;
+
 public interface IRuta {
+    
+    public List listar(); // Método para listar todas las rutas
+    public Ruta obtenerPorId(int id); // Método para obtener una ruta por su ID
+    public boolean editar(Ruta ruta); // Método para editar una ruta
+    public boolean agregar(Ruta ruta); // Método para agregar una nueva ruta
+    public boolean eliminar(int id); // Método para eliminar una ruta por su ID
     
 }
