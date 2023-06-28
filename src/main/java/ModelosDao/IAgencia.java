f9477908574c6d09776abbd995fff25d64a2faf6
Stiven Medina <stvnm33@gmail.com>
2023-06-28 16:38:11 -0500

    Añadiendo interfaz para Agencia

--- src/main/java/ModelosDao/IAgencia.java
+++ src/main/java/ModelosDao/IAgencia.java
@@ -1,5 +1,14 @@
 package ModelosDao;
 
+import Modelos.Agencia;
+import java.util.List;
+
 public interface IAgencia {
+    
+    public List listar(); // Método para listar todas las agencias
+    public Agencia obtenerPorId(int id); // Método para obtener una agencia por su ID
+    public boolean editar(Agencia agencia); // Método para editar una agencia
+    public boolean agregar(Agencia agencia); // Método para agregar una nueva agencia
+    public boolean eliminar(int id); // Método para eliminar una agencia por su ID
     
 }
