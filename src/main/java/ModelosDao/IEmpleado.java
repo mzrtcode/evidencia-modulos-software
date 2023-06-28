00fc3944c89e0ad2b10efdc5ee55f828d87e09b0
Stiven Medina <stvnm33@gmail.com>
2023-06-28 16:38:27 -0500

    Añadiendo interfaz para Empleado

--- src/main/java/ModelosDao/IEmpleado.java
+++ src/main/java/ModelosDao/IEmpleado.java
@@ -1,5 +1,14 @@
 package ModelosDao;
 
+import Modelos.Empleado;
+import java.util.List;
+
 public interface IEmpleado {
+    
+    public List listar(); // Método para listar todas los Empleados
+    public Empleado obtenerPorId(int id); // Método para obtener una Empleado por su ID
+    public boolean editar(Empleado empleado); // Método para editar una Empleado
+    public boolean agregar(Empleado empleado); // Método para agregar un nuevo Empleado
+    public boolean eliminar(int id); // Método para eliminar un Empleado por su ID
     
 }
