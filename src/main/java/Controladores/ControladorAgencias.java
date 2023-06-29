0debd00097cf2e0943c3a0897b287d8abbbb7907
Stiven Medina <stvnm33@gmail.com>
2023-06-29 12:50:07 -0500

    Arreglando el Servelet Agencias

--- src/main/java/Controladores/ControladorAgencias.java
+++ src/main/java/Controladores/ControladorAgencias.java
@@ -1,5 +1,7 @@
 package Controladores;
 
+import Modelos.Agencia;
+import ModelosDao.AgenciaImp;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -10,6 +12,14 @@ import java.io.PrintWriter;
 @WebServlet(name = "ControladorAgencias", value = "/ControladorAgencias")
 public class ControladorAgencias extends HttpServlet {
 
+          // Direcciones de las vistas JSP
+    private final String listar = "vistas/agencias/listar.jsp";
+    private final String add = "vistas/agencias/agregar.jsp";
+    private final String edit = "vistas/agencias/editar.jsp";
+
+    // Instancias de los objetos necesarios
+    private Agencia agencia = new Agencia();
+    private AgenciaImp agenciaImp = new AgenciaImp();
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -48,7 +58,66 @@ public class ControladorAgencias extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String acceso = ""; // Variable para almacenar la página de acceso
+        String accion = request.getParameter("accion"); // Obtener el parámetro "accion" de la solicitud
+
+// Verificar el valor del parámetro "accion" y asignar la página correspondiente a la variable "acceso"
+        if (accion.equalsIgnoreCase("listar")) {
+            acceso = listar; // Página para listar
+        } else if (accion.equalsIgnoreCase("add")) {
+            acceso = add; // Página para agregar
+        } else if (accion.equalsIgnoreCase("agregar")) {
+            // Obtener los valores de los parámetros de inicio y destino de la solicitud
+            String nombre = request.getParameter("txtNombre");
+            String codigoInterno = request.getParameter("txtCodigoInterno");
+            String direccion = request.getParameter("txtDireccion");
+
+
+            // Establecer los valores en el objeto ruta
+            agencia.setNombre(nombre);
+            agencia.setCodigo_interno(codigoInterno);
+            agencia.setDireccion(direccion);
+
+
+
+            // Llamar al método agregar de la clase rutaImp para agregar la ruta a la base de datos
+            agenciaImp.agregar(agencia);
+
+            acceso = listar; // Después de agregar, redirigir a la página de listar
+        }
+
+        else if (accion.equalsIgnoreCase("editar")) {
+            request.setAttribute("idAgencia", request.getParameter("id"));
+            acceso = edit;
+        }else if (accion.equalsIgnoreCase("Actualizar")) {
+            int id = Integer.parseInt(request.getParameter("txtId"));
+            String nombre = request.getParameter("txtNombre");
+            String codigoInterno = request.getParameter("txtCodigoInterno");
+            String direccion = request.getParameter("txtDireccion");
+            boolean estado = request.getParameter("chkEstado") != null && request.getParameter("chkEstado").equals("1");
+
+            AgenciaImp agenciaImp = new AgenciaImp();
+            Agencia agencia = agenciaImp.obtenerPorId(id);
+            agencia.setNombre(nombre);
+            agencia.setCodigo_interno(codigoInterno);
+            agencia.setDireccion(direccion);
+            agencia.setEstado(estado);
+            agenciaImp.editar(agencia);
+
+            String listar = "ControladorAgencias?accion=listar";
+            response.sendRedirect(listar);
+        }
+
+
+        else if (accion.equalsIgnoreCase("eliminar")) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            agencia.setId(id);
+            agenciaImp.eliminar(id);
+            acceso=listar;
+        }
+        RequestDispatcher vista = request.getRequestDispatcher(acceso);
+        vista.forward(request, response);
+
     }
 
     /**
